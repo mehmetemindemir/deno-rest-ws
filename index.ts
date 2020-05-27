3cd7a90a4e13d4d70081bffeed6bdc3acd9b545c
med <eminmehmet.demir@gamil.com>
2020-05-27 15:27:39 -0400

    add Cors

--- index.ts
+++ index.ts
@@ -1,9 +1,9 @@
 import { Application } from "https://deno.land/x/oak/mod.ts";
-
+import { oakCors } from "https://deno.land/x/cors/mod.ts";
 import router from "./routes.ts";
 
 const app = new Application();
-
+app.use(oakCors());
 app.use(router.routes());
 
 await app.listen({ port: 9082 });
